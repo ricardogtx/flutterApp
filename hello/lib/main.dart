0449e172ea5db2986ad413ad16fbc5c306a0f7f8
ricardogtx <ricardocdzg@gmail.com>
2020-08-28 20:57:03 -0300

    First touch with framework

--- hello/lib/main.dart
+++ hello/lib/main.dart
@@ -9,7 +9,34 @@ class Hello extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Material(
-
-        ));
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset(
+            "assets/logo.jpg",
+            width: 90,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text('Hello', style: TextStyle(fontSize: 30)),
+            Padding(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              child: Text('Android', style: TextStyle(fontSize: 30)),
+            ),
+            Text('World', style: TextStyle(fontSize: 30)),
+          ]),
+          FlatButton(
+            onPressed: () => {
+              debugPrint('clicou')
+            },
+            color: Colors.lightGreen,
+            child: Text(
+              'Teste',
+              style: TextStyle(fontSize: 20),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
