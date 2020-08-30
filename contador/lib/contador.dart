40793ed9f4f77e087f78b94b349a439996ab59fe
ricardogtx <ricardocdzg@gmail.com>
2020-08-30 16:34:12 -0300

    Finishing the count app

--- contador/lib/contador.dart
+++ contador/lib/contador.dart
@@ -6,11 +6,53 @@ class Contador extends StatefulWidget {
 }
 
 class _ContadorState extends State<Contador> {
+  var quantidade = 0;
+
+  incrementar(){
+    setState(() {
+      quantidade++;
+    });
+  }
+
+  decrementar(){
+    setState(() {
+      quantidade--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Contador'),
+        backgroundColor: Colors.indigo,
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: Column(
+              children: [
+                ListTile(
+                  title: Text('Quantidade', style: TextStyle(fontSize: 30),)
+                ),
+                Text('$quantidade', style: TextStyle(fontSize: 30),),
+                ButtonBar(
+                  children: [
+                    FlatButton(
+                      child: Text('Incrementar', style: TextStyle(fontSize: 20),),
+                      onPressed: incrementar,
+                    ),
+                    FlatButton(
+                      child: Text('Decrementar', style: TextStyle(fontSize: 20),),
+                      onPressed: decrementar,
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
